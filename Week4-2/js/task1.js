5c505a1989351b9618f90a0005d8b023fbb5b354
yyuueexxiinngg <yyuueexxiinngg@outlook.com>
2017-04-25 15:11:43 +0700

    Finish task

--- Week4-2/js/task1.js
+++ Week4-2/js/task1.js
@@ -1,19 +1,30 @@
- function render(){
-     	var word = "Web 2 is awesome!";
-		document.getElementById("screenX").textContent = window.screenX;
-		document.getElementById("screenY").textContent = window.screenY;
+window.onload = function () {
+    function render() {
+        var word = "Web 2 is awesome!";
+        document.getElementById("screenX").textContent = window.screenX;
+        document.getElementById("screenY").textContent = window.screenY;
         document.getElementById("screenWidth").textContent = window.innerWidth;
         document.getElementById("screenHeight").textContent = window.innerHeight;
         document.getElementById("location").textContent = document.lastModified;
         document.getElementById("myWord").textContent = word;
-        document.getElementById("isItAwesome").textContent = word.indexOf("awesome")>-1?"Yes":"No";
+        document.getElementById("isItAwesome").textContent = word.indexOf("awesome") > -1 ? "Yes" : "No";
 
 
-	};
+    };
 
-var button =  document.getElementById("button");
-window.addEventListener("resize",demo);
-button.addEventListener("click",demo);
-function demo() {
-	render();
+    var button = document.getElementById("button");
+    window.addEventListener("resize",render);
+    button.addEventListener("click",render);
+    
+    
+    function devInfo() {
+        var ele = document.getElementById("dev-info");
+        var info = document.getElementById("myinfo");
+        var innerText = "Andrew<br/>IT student<br/><a href='https://yyuueexxiinngg.github.io/ITE220/' target='_blank'>https://yyuueexxiinngg.github.io/ITE220/</a>";
+        ele.addEventListener("click",function () {
+            info.innerHTML = innerText;
+        })
+    }
+
+    devInfo();
 }
